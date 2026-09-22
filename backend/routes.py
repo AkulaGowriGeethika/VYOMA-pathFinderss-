deea38537dc8a0ca566fc92c4fc9b0030a117eb7
Gowri Geethika Sai Srija Akula <a.geethika1507@gmail.com>
2026-09-22 21:41:09 +0530

    Delete backend directory

--- backend/routes.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from schemas import SensorData
-from navigation import navigate
-
-router = APIRouter()
-
-@router.post("/sensor")
-def sensor_data(data: SensorData):
-    return navigate(data)
